036c49b6d62c347fccd140ecb1303c91726e3abf
YSG <dbtjrrbs12@naver.com>
2025-03-23 21:13:14 +0900

    Add : 패키지 구조에 맞게 프로젝트 생성

---
+++ src/main/java/com/bbf/bebefriends/BebefriendsApplication.java
@@ -0,0 +1,14 @@
+package com.bbf.bebefriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class BebefriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebefriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbf/bebefriends/BebefriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbf.bebefriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebefriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
