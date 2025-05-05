bcb897e71eb86aec8820669a157baa393e4d2271
hojooo <ghwn5833@gmail.com>
2025-05-05 21:09:05 +0900

    docs: api endpoint에 대한 swagger 설명 추가

--- src/main/java/com/bbf/bebefriends/community/controller/CommunityCategoryController.java
+++ src/main/java/com/bbf/bebefriends/community/controller/CommunityCategoryController.java
@@ -6,6 +6,10 @@ import com.bbf.bebefriends.community.service.CommunityCategoryService;
 import com.bbf.bebefriends.global.entity.BaseResponse;
 import com.bbf.bebefriends.global.entity.UserDetailsImpl;
 import com.bbf.bebefriends.global.exception.ResponseCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +19,20 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "커뮤니티 게시물 카테고리 api", description = "전체 카테고리 조회·카테고리 생성·수정 api")
 public class CommunityCategoryController {
     private final CommunityCategoryService communityCategoryService;
 
     // 전체 카테고리 조회
     @GetMapping("/community-category")
+    @Operation(summary = "전체 카테고리 조회", description = "전체 카테고리를 조회합니다.")
     public BaseResponse<List<CommunityCategory>> getAllCategory() {
         return BaseResponse.onSuccess(communityCategoryService.getAllCategory(), ResponseCode.OK);
     }
 
     // 카테고리 만들기
     @PostMapping("/community-category")
+    @Operation(summary = "카테고리 생성", description = "카테고리를 생성합니다.(관리자만 가능)")
     public BaseResponse<CommunityCategoryDTO.CreateCategoryResponse> createCategory(CommunityCategoryDTO.CreateCategoryRequest request,
                                                                                       @AuthenticationPrincipal UserDetailsImpl user) {
         return BaseResponse.onSuccess(communityCategoryService.saveCategory(request, user.getUser()), ResponseCode.OK);
@@ -33,6 +40,7 @@ public class CommunityCategoryController {
 
     // 카테고리 수정
     @PatchMapping("/community-category")
+    @Operation(summary = "카테고리 이름 수정", description = "카테고리 이름을 수정합니다.(관리자만 가능)")
     public BaseResponse<CommunityCategoryDTO.UpdateCategoryResponse> updateCategory(CommunityCategoryDTO.UpdateCategoryRequest request,
                                                                                     @AuthenticationPrincipal UserDetailsImpl user) {
         return BaseResponse.onSuccess(communityCategoryService.updateCategory(request, user.getUser()), ResponseCode.OK);
--- src/main/java/com/bbf/bebefriends/community/controller/CommunityPostListController.java
+++ src/main/java/com/bbf/bebefriends/community/controller/CommunityPostListController.java
@@ -5,6 +5,8 @@ import com.bbf.bebefriends.community.service.CommunityPostListService;
 import com.bbf.bebefriends.global.entity.BaseResponse;
 import com.bbf.bebefriends.global.entity.UserDetailsImpl;
 import com.bbf.bebefriends.global.exception.ResponseCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,47 +19,55 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "게시물 목록 api", description = "모든 게시물·카테고리·제목·작성자·내가 쓴 게시물·댓글 단 게시물·좋아요한 게시물 목록 api")
 public class CommunityPostListController {
     private final CommunityPostListService communityPostListService;
 
     // 모든 게시물
     @GetMapping("/all-posts")
+    @Operation(summary = "전체 게시물 목록", description = "전체 게시물을 조회합니다.(아무런 인증 필요 없음)")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getPosts() {
         return BaseResponse.onSuccess(communityPostListService.getAllPosts(), ResponseCode.OK);
     }
 
     // 카테고리 별 조회
     @GetMapping("/category-posts")
+    @Operation(summary = "카테고리 별 게시물 목록", description = "카테고리 별로 게시물 목록을 조회합니다.")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getCategoryPosts(@RequestParam String category) {
         return BaseResponse.onSuccess(communityPostListService.getPostsByCategory(category), ResponseCode.OK);
     }
 
     // 제목 조회
     @GetMapping("/search/title")
+    @Operation(summary = "제목 검색", description = "제목을 검색하여 게시물 목록을 조회합니다.")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getTitleSearch(@RequestParam String title) {
         return BaseResponse.onSuccess(communityPostListService.getPostsByTitle(title), ResponseCode.OK);
     }
 
     // 작성자 조회
     @GetMapping("/search/author")
+    @Operation(summary = "작성자 검색", description = "작성자를 검색하여 게시물 목록을 조회합니다.")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getAuthorSearch(@RequestParam String author) {
         return BaseResponse.onSuccess(communityPostListService.getPostsByAuthor(author), ResponseCode.OK);
     }
 
     // 내가 쓴 게시물
     @GetMapping("/my-posts")
+    @Operation(summary = "내가 쓴 게시물", description = "내가 쓴 게시물을 조회합니다.")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getMyPosts(@AuthenticationPrincipal UserDetailsImpl user) {
         return BaseResponse.onSuccess(communityPostListService.getMyPosts(user.getUser()), ResponseCode.OK);
     }
 
     // 댓글 단 게시물
     @GetMapping("/commented-posts")
+    @Operation(summary = "내가 댓글 단 게시물", description = "내가 댓글 단 게시물을 조회합니다.")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getCommentedPosts(@AuthenticationPrincipal UserDetailsImpl user) {
         return BaseResponse.onSuccess(communityPostListService.getCommentedPosts(user.getUser()), ResponseCode.OK);
     }
 
     // 좋아요한 게시물
     @GetMapping("/liked-posts")
+    @Operation(summary = "내가 좋아요한 게시물", description = "내가 좋아요한 게시물을 조회합니다.")
     public BaseResponse<List<CommunityPostDTO.PostListResponse>> getLikedPosts(@AuthenticationPrincipal UserDetailsImpl user) {
         return BaseResponse.onSuccess(communityPostListService.getLikedPosts(user.getUser()), ResponseCode.OK);
     }
