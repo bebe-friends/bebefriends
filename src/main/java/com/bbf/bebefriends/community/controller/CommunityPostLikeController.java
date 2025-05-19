d303aea27f6744a4d7f40c985ce4e9b26c883838
hojooo <ghwn5833@gmail.com>
2025-05-19 16:57:49 +0900

    fix: 커뮤니티 게시물 좋아요 api 하나로 통합

--- src/main/java/com/bbf/bebefriends/community/controller/CommunityPostLikeController.java
+++ src/main/java/com/bbf/bebefriends/community/controller/CommunityPostLikeController.java
@@ -5,9 +5,11 @@ import com.bbf.bebefriends.community.entity.CommunityPostLike;
 import com.bbf.bebefriends.community.service.CommunityPostLikeService;
 import com.bbf.bebefriends.community.service.CommunityPostListService;
 import com.bbf.bebefriends.global.entity.BaseResponse;
+import com.bbf.bebefriends.global.entity.UserDetailsImpl;
 import com.bbf.bebefriends.global.exception.ResponseCode;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,14 +22,14 @@ public class CommunityPostLikeController {
     // 게시물 좋아요
     @PostMapping("/post-like/{postId}")
     public BaseResponse<String> createPostLike(@PathVariable Long postId,
-                                               @RequestBody CommunityPostLikeDTO.PostLikeRequest request) {
-        return BaseResponse.onSuccess(communityPostLikeService.createPostLike(postId, request), ResponseCode.OK);
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return BaseResponse.onSuccess(communityPostLikeService.updatePostLike(postId, userDetails.getUser()), ResponseCode.OK);
     }
 
-    // 게시물 좋아요 취소
-    @DeleteMapping("/post-like/{postId}")
-    public BaseResponse<String> deletePostLike(@PathVariable Long postId,
-                                               @RequestBody CommunityPostLikeDTO.PostLikeRequest request) {
-        return BaseResponse.onSuccess(communityPostLikeService.deletePostLike(postId, request), ResponseCode.OK);
-    }
+//    // 게시물 좋아요 취소
+//    @DeleteMapping("/post-like/{postId}")
+//    public BaseResponse<String> deletePostLike(@PathVariable Long postId,
+//                                               @RequestBody CommunityPostLikeDTO.PostLikeRequest request) {
+//        return BaseResponse.onSuccess(communityPostLikeService.deletePostLike(postId, request), ResponseCode.OK);
+//    }
 }
