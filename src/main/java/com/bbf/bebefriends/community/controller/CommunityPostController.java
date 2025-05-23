2f585f38a4fccbd69a6950e87dc154a68b8b0027
hojooo <ghwn5833@gmail.com>
2025-05-23 17:41:00 +0900

    fix: controller uri 경로 수정

--- src/main/java/com/bbf/bebefriends/community/controller/CommunityPostController.java
+++ src/main/java/com/bbf/bebefriends/community/controller/CommunityPostController.java
@@ -13,40 +13,42 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/community-post")
 @Tag(name = "커뮤니티 게시글 api", description = "게시물 생성·수정·삭제·상세 페이지 관련 API")
 public class CommunityPostController {
     private final CommunityPostService communityPostService;
 
     // FIXME: multipartfile 부분 수정 필요
     // 게시글 작성
-    @PostMapping(
-            value = "/post",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
-    )
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "커뮤니티 게시글 생성", description = "게시물을 게시합니다.")
-    public BaseResponse<CommunityPostDTO.CreatePostResponse> createCommunityPost(@Valid @ModelAttribute CommunityPostDTO.CreatePostRequest request,
-                                                                                 @AuthenticationPrincipal UserDetailsImpl user) {
-
-        return BaseResponse.onSuccess(communityPostService.createPost(request, user.getUser()), ResponseCode.OK);
+    public BaseResponse<CommunityPostDTO.CreatePostResponse> createCommunityPost(
+            @RequestPart("data") @Valid CommunityPostDTO.CreatePostRequest request,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images,
+            @AuthenticationPrincipal UserDetailsImpl user
+    ) {
+        return BaseResponse.onSuccess(communityPostService.createPost(request, images, user.getUser()), ResponseCode.OK);
     }
 
     // 게시글 수정
-    @PatchMapping(
-            value = "/post",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
-    )
+    @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "커뮤니티 게시글 수정", description = "본인이 작성한 게시물을 수정합니다.")
-    public BaseResponse<CommunityPostDTO.UpdatePostResponse> updatePost(@Valid @ModelAttribute CommunityPostDTO.UpdatePostRequest request,
-                                                                          @AuthenticationPrincipal UserDetailsImpl user) {
-        return BaseResponse.onSuccess(communityPostService.updatePost(request, user.getUser()), ResponseCode.OK);
+    public BaseResponse<CommunityPostDTO.UpdatePostResponse> updatePost(
+            @RequestPart("data") @Valid CommunityPostDTO.UpdatePostRequest request,
+            @RequestPart(value = "newImages", required = false) List<MultipartFile> newImages,
+            @AuthenticationPrincipal UserDetailsImpl user
+    ) {
+        return BaseResponse.onSuccess(communityPostService.updatePost(request, newImages, user.getUser()), ResponseCode.OK);
     }
 
     // 게시글 삭제
-    @DeleteMapping("/post")
+    @DeleteMapping()
     @Operation(summary = "커뮤니티 게시글 삭제", description = "본인이 작성한 게시물을 삭제합니다.")
     public BaseResponse<String> deletePost(@Valid @RequestBody CommunityPostDTO.DeletePostRequest request,
                                              @AuthenticationPrincipal UserDetailsImpl user) {
@@ -54,8 +56,8 @@ public class CommunityPostController {
     }
 
     // 게시글 상세 페이지
-    @GetMapping("/post-detail")
-    @Operation(summary = "커뮤니티 게시글 상세 페이자", description = "게시글 상세 페이지를 조회합니다.")
+    @GetMapping("/detail")
+    @Operation(summary = "커뮤니티 게시글 상세 페이지", description = "게시글 상세 페이지를 조회합니다.")
     public BaseResponse<CommunityPostDTO.PostDetailsResponse>  getPostDetails(@AuthenticationPrincipal UserDetailsImpl user,
                                                                               @RequestParam Long postId) {
         return BaseResponse.onSuccess(communityPostService.getPostDetail(postId), ResponseCode.OK);
