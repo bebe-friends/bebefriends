5f47b84b286bb14cbbd78151e11146cf5e2ac7a5
ysg <dbtjrrbs12@naver.com>
2025-05-31 19:09:57 +0900

    Refactor : 유저 값 가져오도록 수정

--- src/main/java/com/bbf/bebefriends/hotdeal/controller/HotDealPostController.java
+++ src/main/java/com/bbf/bebefriends/hotdeal/controller/HotDealPostController.java
@@ -1,12 +1,12 @@
 package com.bbf.bebefriends.hotdeal.controller;
 
 import com.bbf.bebefriends.global.entity.BaseResponse;
+import com.bbf.bebefriends.global.entity.UserDetailsImpl;
 import com.bbf.bebefriends.global.exception.ResponseCode;
 import com.bbf.bebefriends.hotdeal.dto.HotDealCommentDto;
 import com.bbf.bebefriends.hotdeal.dto.HotDealLikeDto;
 import com.bbf.bebefriends.hotdeal.dto.HotDealPostDto;
 import com.bbf.bebefriends.hotdeal.service.HotDealPostService;
-import com.bbf.bebefriends.member.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -37,29 +37,29 @@ public class HotDealPostController {
 
     @Operation(summary = "핫딜 게시글 등록", description = "핫딜 게시글을 등록합니다.")
     @PostMapping
-    public BaseResponse<HotDealPostDto> createHotDealPost(@RequestBody HotDealPostDto hotDealPostDto, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.createHotDealPost(hotDealPostDto, user), ResponseCode.OK);
+    public BaseResponse<HotDealPostDto> createHotDealPost(@RequestBody HotDealPostDto hotDealPostDto, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.createHotDealPost(hotDealPostDto, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 게시글 수정", description = "핫딜 게시글을 수정합니다.")
     @PutMapping
-    public BaseResponse<HotDealPostDto> updateHotDealPost(@RequestBody HotDealPostDto hotDealPostDto, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.updateHotDealPost(hotDealPostDto, user), ResponseCode.OK);
+    public BaseResponse<HotDealPostDto> updateHotDealPost(@RequestBody HotDealPostDto hotDealPostDto, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.updateHotDealPost(hotDealPostDto, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 게시글 삭제", description = "핫딜 게시글을 삭제합니다.")
     @DeleteMapping
-    public BaseResponse<Long> deleteHotDealPost(@RequestParam Long hotDealPostId, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.deleteHotDealPost(hotDealPostId, user), ResponseCode.OK);
+    public BaseResponse<Long> deleteHotDealPost(@RequestParam Long hotDealPostId, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.deleteHotDealPost(hotDealPostId, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 게시글 상세 조회", description = "핫딜 게시글을 상세 조회합니다.")
     @GetMapping("/detail")
-    public BaseResponse<HotDealPostDto> searchHotDealPostDetail(@RequestParam Long hotDealPostId, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.searchHotDealPostDetail(hotDealPostId, user), ResponseCode.OK);
+    public BaseResponse<HotDealPostDto> searchHotDealPostDetail(@RequestParam Long hotDealPostId, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.searchHotDealPostDetail(hotDealPostId, user.getUser()), ResponseCode.OK);
 
     }
 
@@ -71,36 +71,36 @@ public class HotDealPostController {
 
     @Operation(summary = "핫딜 댓글 등록", description = "핫딜 게시글의 댓글을 등록합니다.")
     @PostMapping("/comment")
-    public BaseResponse<HotDealCommentDto> createHotDealComment(@RequestBody HotDealCommentDto hotDealCommentDto, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.createHotDealComment(hotDealCommentDto, user), ResponseCode.OK);
+    public BaseResponse<HotDealCommentDto> createHotDealComment(@RequestBody HotDealCommentDto hotDealCommentDto, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.createHotDealComment(hotDealCommentDto, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 댓글 수정", description = "핫딜 게시글의 댓글을 수정합니다.")
     @PutMapping("/comment")
-    public BaseResponse<HotDealCommentDto> updateHotDealComment(@RequestBody HotDealCommentDto hotDealCommentDto, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.updateHotDealComment(hotDealCommentDto, user), ResponseCode.OK);
+    public BaseResponse<HotDealCommentDto> updateHotDealComment(@RequestBody HotDealCommentDto hotDealCommentDto, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.updateHotDealComment(hotDealCommentDto, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 댓글 삭제", description = "핫딜 게시글의 댓글을 삭제합니다.")
     @DeleteMapping("/comment")
-    public BaseResponse<Long> deleteHotDealComment(@RequestParam Long hotDealCommentId, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.deleteHotDealComment(hotDealCommentId, user), ResponseCode.OK);
+    public BaseResponse<Long> deleteHotDealComment(@RequestParam Long hotDealCommentId, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.deleteHotDealComment(hotDealCommentId, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 좋아요", description = "핫딜 게시글을 좋아요 하거나 좋아요 취소합니다.")
     @PostMapping("/like")
-    public BaseResponse<HotDealLikeDto> likeHotDealPost(@RequestParam Long hotDealPostId, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.likeHotDealPost(hotDealPostId, user), ResponseCode.OK);
+    public BaseResponse<HotDealLikeDto> likeHotDealPost(@RequestParam Long hotDealPostId, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.likeHotDealPost(hotDealPostId, user.getUser()), ResponseCode.OK);
 
     }
 
     @Operation(summary = "핫딜 좋아요 여부 조회", description = "핫딜 게시글을 좋아요가 되어있는지 여부를 조회합니다.")
     @GetMapping("/like")
-    public BaseResponse<HotDealLikeDto> likeHotDealPostChk(@RequestParam Long hotDealPostId, @AuthenticationPrincipal User user) {
-        return BaseResponse.onSuccess(hotDealPostService.likeHotDealPostChk(hotDealPostId, user), ResponseCode.OK);
+    public BaseResponse<HotDealLikeDto> likeHotDealPostChk(@RequestParam Long hotDealPostId, @AuthenticationPrincipal UserDetailsImpl user) {
+        return BaseResponse.onSuccess(hotDealPostService.likeHotDealPostChk(hotDealPostId, user.getUser()), ResponseCode.OK);
 
     }
 
