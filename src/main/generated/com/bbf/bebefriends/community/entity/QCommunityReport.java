987b7c1fa3609ef4b0b55a5ad043dc2354f1d253
JJungwoo <jungwoo5759@gmail.com>
2025-06-20 22:17:48 +0900

    remove : 핫딜 서비스 관련 구현 삭제

--- src/main/generated/com/bbf/bebefriends/community/entity/QCommunityReport.java
+++ src/main/generated/com/bbf/bebefriends/community/entity/QCommunityReport.java
@@ -11,18 +11,22 @@ import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
- * QCommunityPostBlock is a Querydsl query type for CommunityPostBlock
+ * QCommunityReport is a Querydsl query type for CommunityReport
  */
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
-public class QCommunityPostBlock extends EntityPathBase<CommunityPostBlock> {
+public class QCommunityReport extends EntityPathBase<CommunityReport> {
 
-    private static final long serialVersionUID = 1192088342L;
+    private static final long serialVersionUID = -1062859125L;
 
     private static final PathInits INITS = PathInits.DIRECT2;
 
-    public static final QCommunityPostBlock communityPostBlock = new QCommunityPostBlock("communityPostBlock");
+    public static final QCommunityReport communityReport = new QCommunityReport("communityReport");
 
     public final com.bbf.bebefriends.global.entity.QBaseEntity _super = new com.bbf.bebefriends.global.entity.QBaseEntity(this);
+
+    public final QCommunityComment comment;
+
+    public final StringPath content = createString("content");
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
@@ -31,29 +35,32 @@ public class QCommunityPostBlock extends EntityPathBase<CommunityPostBlock> {
 
     public final QCommunityPost post;
 
+    public final EnumPath<BlockReason> reason = createEnum("reason", BlockReason.class);
+
     //inherited
     public final DateTimePath<java.time.LocalDateTime> updatedDate = _super.updatedDate;
 
     public final com.bbf.bebefriends.member.entity.QUser user;
 
-    public QCommunityPostBlock(String variable) {
-        this(CommunityPostBlock.class, forVariable(variable), INITS);
+    public QCommunityReport(String variable) {
+        this(CommunityReport.class, forVariable(variable), INITS);
     }
 
-    public QCommunityPostBlock(Path<? extends CommunityPostBlock> path) {
+    public QCommunityReport(Path<? extends CommunityReport> path) {
         this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
-    public QCommunityPostBlock(PathMetadata metadata) {
+    public QCommunityReport(PathMetadata metadata) {
         this(metadata, PathInits.getFor(metadata, INITS));
     }
 
-    public QCommunityPostBlock(PathMetadata metadata, PathInits inits) {
-        this(CommunityPostBlock.class, metadata, inits);
+    public QCommunityReport(PathMetadata metadata, PathInits inits) {
+        this(CommunityReport.class, metadata, inits);
     }
 
-    public QCommunityPostBlock(Class<? extends CommunityPostBlock> type, PathMetadata metadata, PathInits inits) {
+    public QCommunityReport(Class<? extends CommunityReport> type, PathMetadata metadata, PathInits inits) {
         super(type, metadata, inits);
+        this.comment = inits.isInitialized("comment") ? new QCommunityComment(forProperty("comment"), inits.get("comment")) : null;
         this.post = inits.isInitialized("post") ? new QCommunityPost(forProperty("post"), inits.get("post")) : null;
         this.user = inits.isInitialized("user") ? new com.bbf.bebefriends.member.entity.QUser(forProperty("user"), inits.get("user")) : null;
     }
--- src/main/generated/com/bbf/bebefriends/community/entity/QCommunityUserBlock.java
+++ src/main/generated/com/bbf/bebefriends/community/entity/QCommunityUserBlock.java
@@ -11,50 +11,50 @@ import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
- * QCommunityPostBlock is a Querydsl query type for CommunityPostBlock
+ * QCommunityUserBlock is a Querydsl query type for CommunityUserBlock
  */
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
-public class QCommunityPostBlock extends EntityPathBase<CommunityPostBlock> {
+public class QCommunityUserBlock extends EntityPathBase<CommunityUserBlock> {
 
-    private static final long serialVersionUID = 1192088342L;
+    private static final long serialVersionUID = -471350581L;
 
     private static final PathInits INITS = PathInits.DIRECT2;
 
-    public static final QCommunityPostBlock communityPostBlock = new QCommunityPostBlock("communityPostBlock");
+    public static final QCommunityUserBlock communityUserBlock = new QCommunityUserBlock("communityUserBlock");
 
     public final com.bbf.bebefriends.global.entity.QBaseEntity _super = new com.bbf.bebefriends.global.entity.QBaseEntity(this);
+
+    public final com.bbf.bebefriends.member.entity.QUser blockedUser;
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
 
     public final NumberPath<Long> id = createNumber("id", Long.class);
 
-    public final QCommunityPost post;
-
     //inherited
     public final DateTimePath<java.time.LocalDateTime> updatedDate = _super.updatedDate;
 
     public final com.bbf.bebefriends.member.entity.QUser user;
 
-    public QCommunityPostBlock(String variable) {
-        this(CommunityPostBlock.class, forVariable(variable), INITS);
+    public QCommunityUserBlock(String variable) {
+        this(CommunityUserBlock.class, forVariable(variable), INITS);
     }
 
-    public QCommunityPostBlock(Path<? extends CommunityPostBlock> path) {
+    public QCommunityUserBlock(Path<? extends CommunityUserBlock> path) {
         this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
-    public QCommunityPostBlock(PathMetadata metadata) {
+    public QCommunityUserBlock(PathMetadata metadata) {
         this(metadata, PathInits.getFor(metadata, INITS));
     }
 
-    public QCommunityPostBlock(PathMetadata metadata, PathInits inits) {
-        this(CommunityPostBlock.class, metadata, inits);
+    public QCommunityUserBlock(PathMetadata metadata, PathInits inits) {
+        this(CommunityUserBlock.class, metadata, inits);
     }
 
-    public QCommunityPostBlock(Class<? extends CommunityPostBlock> type, PathMetadata metadata, PathInits inits) {
+    public QCommunityUserBlock(Class<? extends CommunityUserBlock> type, PathMetadata metadata, PathInits inits) {
         super(type, metadata, inits);
-        this.post = inits.isInitialized("post") ? new QCommunityPost(forProperty("post"), inits.get("post")) : null;
+        this.blockedUser = inits.isInitialized("blockedUser") ? new com.bbf.bebefriends.member.entity.QUser(forProperty("blockedUser"), inits.get("blockedUser")) : null;
         this.user = inits.isInitialized("user") ? new com.bbf.bebefriends.member.entity.QUser(forProperty("user"), inits.get("user")) : null;
     }
 
